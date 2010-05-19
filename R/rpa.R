bc4e23dd16577c25eae2b9c3e19a14449efec54c
Leo Lahti <leo.lahti@tkk.fi>
2010-05-19 13:15:57 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RPA@46983 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/rpa.R
+++ R/rpa.R
@@ -1,5 +1,5 @@
 
-                                        #
+#                                        
 # This file is a part of the RPA program (Robust Probabilistic
 # Averaging), see http://www.cis.hut.fi/projects/mi/software/RPA/
 #
@@ -7,15 +7,18 @@
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
+# the Free Software Foundation, either version 2, or (at your option)
 # any later version.
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License 2 for more details.
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License 2 for more details.
 # 
-
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+#
+  
 rpa <- function (abatch,
                     sets = NULL,
                   myseed = 101, 

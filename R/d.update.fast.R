29da1e157a81d0673b49e530e82c66505797a9bb
Leo Lahti <leo.lahti@tkk.fi>
2013-02-12 12:37:23 +0000

    exporting d.update.fast
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RPA@73406 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/d.update.fast.R
+++ R/d.update.fast.R
@@ -2,7 +2,7 @@
 # (Robust Probabilistic Averaging) 
 # http://bioconductor.org/packages/release/bioc/html/RPA.html
 
-# Copyright (C) 2008-2012 Leo Lahti <leo.lahti@iki.fi>. All rights reserved.
+# Copyright (C) 2008-2013 Leo Lahti <leo.lahti@iki.fi>. All rights reserved.
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the FreeBSD License.
@@ -11,9 +11,25 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 
-d.update.fast <- function (St, s2) {
 
-  # St: probes x arrays  
+#' d.update.fast
+#'
+#' Description: computes weighted average over the probes, 
+#' weighted by their inverse probe-specific variances.
+#'
+#' @param St probes x samples data matrix
+#' @param s2 variances for the probes
+#'
+#' @details Returns summarized probeset-level weighted average
+#'
+#' @export
+#'
+#' @references See citation("RPA") 
+#' @author Leo Lahti \email{leo.lahti@@iki.fi}
+#' @examples # 
+#' @keywords methods
+
+d.update.fast <- function (St, s2) {
 
   # With large sample sizes when T -> Inf
   # d converges to the weighted mean 

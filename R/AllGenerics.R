ae1cbb754568545b9d32507cc5ebb10c86d4d973
Leo Lahti <leo.lahti@tkk.fi>
2013-03-05 22:19:04 +0000

    syncing with github2
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RPA@73943 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllGenerics.R
+++
@@ -1,6 +0,0 @@
-#setGeneric("show", function(object) { standardGeneric("show") })
-#setGeneric("[",function(x,i,j,drop){standardGeneric("[")})
-#setGeneric("[[",function(x,i,j,drop){standardGeneric("[[")})
-NULL
-
-

9ac03d9d9762861ea94d383ee9f123c55a2132ff
Leo Lahti <leo.lahti@tkk.fi>
2013-10-20 10:30:12 +0000

    vignette and citation update
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RPA@81891 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/firstlib.R
+++ R/firstlib.R
@@ -1,6 +1,4 @@
-.onLoad <- function(lib, pkg)
+.onAttach <- function(lib, pkg)
 {
-   #library.dynam('RPA', pkg, lib)
    packageStartupMessage("\nRPA Copyright (C) 2008-2013 Leo Lahti.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under the FreeBSD open source license.\n")
-
 }

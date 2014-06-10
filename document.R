5ff3aa9663429855e1fee42548ef2bda8716f55f
Leo Lahti <leo.lahti@tkk.fi>
2014-06-10 18:39:49 +0000

    setting up git-svn bridge
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/RPA@91151 bc3139a8-67e5-0310-9ffc-ced21a209358

--- document.R
+++
@@ -1,2 +0,0 @@
-library(devtools)
-document("pkg", clean = TRUE)
--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,6 +1,5 @@
-
 /usr/bin/R CMD BATCH document.R
-/usr/bin/R CMD check pkg
-/usr/bin/R CMD build pkg
+/usr/bin/R CMD build ../../
+/usr/bin/R CMD check RPA_1.21.11.tar.gz
 /usr/bin/R CMD INSTALL RPA_1.21.11.tar.gz
 

fd8cefe81a2229549d7e51ccc6c5a8b332a0e5fa
antagomir <leo.lahti@iki.fi>
2017-11-12 15:52:36 +0100

    testing bioc sync

--- R/firstlib.R
+++ R/firstlib.R
@@ -4,5 +4,5 @@
 
 .onAttach <- function(lib, pkg)
 {
-   packageStartupMessage("\nRPA Copyright (C) 2008-2016 Leo Lahti.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under the FreeBSD open source license.\n")
+   packageStartupMessage("\nRPA Copyright (C) 2008-2017 Leo Lahti.\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under the FreeBSD open source license.\n")
 }
--- inst/extras/build.sh
+++ inst/extras/build.sh
@@ -1,5 +1,5 @@
 /usr/bin/R CMD BATCH document.R
 /usr/bin/R CMD build ../../
-/usr/bin/R CMD check RPA_1.27.41.tar.gz
-/usr/bin/R CMD INSTALL RPA_1.27.41.tar.gz
+/usr/bin/R CMD check RPA_1.35.1.tar.gz
+/usr/bin/R CMD INSTALL RPA_1.35.1.tar.gz
 
